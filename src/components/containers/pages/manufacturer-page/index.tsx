9721009989a5eb156c938e4f6954f63f2c51c616
donreinardgomez <Don.Reinard.Gomez@jp.ey.com>
2022-03-03 09:28:38 +0900

    Added sample man page imp

--- src/components/containers/pages/manufacturer-page/index.tsx
+++ src/components/containers/pages/manufacturer-page/index.tsx
@@ -1,16 +1,26 @@
 import { IStore } from '@models/store';
+import { TextDisplay } from '@visuals/text-display';
 import React from 'react';
 import { connect } from 'react-redux';
 import { RouteComponentProps } from 'react-router-dom';
 import { compose } from 'redux';
+import styles from './style.scss';
 
 const ManufacturerPageFC: React.FC<Props> = ({ state, match }) => {
   const targetManId = Number(match?.params['id']);
   const displayManufacturer = state?.data?.manufacturers.find((man) => man.id === targetManId);
   return (
-    <div>
-      <div>{JSON.stringify(displayManufacturer)}</div>
-      <div>Items</div>
+    <div className={styles.container}>
+      <TextDisplay size='large' weight='bold'>
+        INFO
+      </TextDisplay>
+      <div>
+        <TextDisplay weight='bold'>Name:</TextDisplay>
+        <TextDisplay weight='bold'>{displayManufacturer.name}</TextDisplay>
+      </div>
+      <TextDisplay size='large' weight='bold'>
+        ITEM
+      </TextDisplay>
     </div>
   );
 };
