6bd45d041356b77a93f55d28ba8627efa43e8cbc
luomin <min.luo@jp.ey.com>
2022-03-03 22:19:01 +0900

    tiny change in manufacture page

--- src/components/containers/pages/manufacturer-page/index.tsx
+++ src/components/containers/pages/manufacturer-page/index.tsx
@@ -12,68 +12,97 @@ const ManufacturerPageFC: React.FC<Props> = ({ state, match }) => {
   const displayManufacturer = state?.data?.manufacturers.find((man) => man.id === targetManId);
   return (
     <div className={styles.container}>
-      <TextDisplay size='xx-large' weight='bold'>
-        Manufacture Company Information:
-      </TextDisplay>
+      <div className={styles.summary}>
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '20px',
+          }}
+        >
+          <TextDisplay size='xx-large' weight='bold'>
+            Manufacture Company Information:
+          </TextDisplay>
+        </div>
 
-      <TextDisplay size='xxx-large' weight='bold' color='primary-1'>
-        NAKAYAMA
-      </TextDisplay>
-
-      <div>
-        <TextDisplay size='x-large' weight='bold'>
-          Manufacture Name:
-        </TextDisplay>
-        <TextDisplay weight='normal'>{displayManufacturer.name}</TextDisplay>
-
-        <div className={styles.items}>
-          <TextDisplay weight='normal'>Hmepage: {displayManufacturer.homepage}</TextDisplay>
-          <TextDisplay weight='normal'>Awards: {displayManufacturer.awards}</TextDisplay>
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '50px',
+          }}
+        >
+          <TextDisplay size='xxx-large' weight='bold' color='primary-1'>
+            {displayManufacturer.name}
+          </TextDisplay>
         </div>
       </div>
-      <div>
-        <TextDisplay size='large' weight='bold'>
-          Product Items:
-        </TextDisplay>
-      </div>
-      <TextDisplay weight='normal'>Item 1</TextDisplay>
-      <div className={styles.items}>
-        <TextDisplay weight='normal'>Name: {displayManufacturer.items[0].name}</TextDisplay>
-        <TextDisplay weight='normal'>
-          Made in: {displayManufacturer.items[0].originCountry}
-        </TextDisplay>
-        <TextDisplay weight='normal'>
-          Green Points: {formatNumber(displayManufacturer.items[0].greenPoint)}
-        </TextDisplay>
-        <TextDisplay weight='normal'>
-          Description: {displayManufacturer.items[0].description}
-        </TextDisplay>
-      </div>
-      <TextDisplay weight='normal'>Item 2</TextDisplay>
-      <div className={styles.items}>
-        <TextDisplay weight='normal'>Name: {displayManufacturer.items[1].name}</TextDisplay>
-        <TextDisplay weight='normal'>
-          Made in: {displayManufacturer.items[1].originCountry}
-        </TextDisplay>
-        <TextDisplay weight='normal'>
-          Green Points: {formatNumber(displayManufacturer.items[1].greenPoint)}
-        </TextDisplay>
-        <TextDisplay weight='normal'>
-          Description: {displayManufacturer.items[1].description}
-        </TextDisplay>
-      </div>
-      <TextDisplay weight='normal'>Item 3</TextDisplay>
-      <div className={styles.items}>
-        <TextDisplay weight='normal'>Name: {displayManufacturer.items[2].name}</TextDisplay>
-        <TextDisplay weight='normal'>
-          Made in: {displayManufacturer.items[2].originCountry}
-        </TextDisplay>
-        <TextDisplay weight='normal'>
-          Green Points: {formatNumber(displayManufacturer.items[2].greenPoint)}
-        </TextDisplay>
-        <TextDisplay weight='normal'>
-          Description: {displayManufacturer.items[2].description}
-        </TextDisplay>
+
+      <div className={styles.table}>
+        <div>
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              height: '50px',
+            }}
+          >
+            <TextDisplay size='large' weight='bold'>
+              Product Item List:
+            </TextDisplay>
+          </div>
+        </div>
+
+        <div className={styles.table}>
+          <TextDisplay weight='normal'>Item 1</TextDisplay>
+          <div className={styles.items}>
+            <TextDisplay weight='normal'>Name: {displayManufacturer.items[0].name}</TextDisplay>
+            <TextDisplay weight='normal'>
+              Made in: {displayManufacturer.items[0].originCountry}
+            </TextDisplay>
+            <TextDisplay weight='normal'>
+              Green Points: {formatNumber(displayManufacturer.items[0].greenPoint)}
+            </TextDisplay>
+            <TextDisplay weight='normal'>
+              Description: {displayManufacturer.items[0].description}
+            </TextDisplay>
+          </div>
+        </div>
+
+        <div className={styles.table}>
+          <TextDisplay weight='normal'>Item 2</TextDisplay>
+          <div className={styles.items}>
+            <TextDisplay weight='normal'>Name: {displayManufacturer.items[1].name}</TextDisplay>
+            <TextDisplay weight='normal'>
+              Made in: {displayManufacturer.items[1].originCountry}
+            </TextDisplay>
+            <TextDisplay weight='normal'>
+              Green Points: {formatNumber(displayManufacturer.items[1].greenPoint)}
+            </TextDisplay>
+            <TextDisplay weight='normal'>
+              Description: {displayManufacturer.items[1].description}
+            </TextDisplay>
+          </div>
+        </div>
+
+        <div className={styles.table}>
+          <TextDisplay weight='normal'>Item 3</TextDisplay>
+          <div className={styles.items}>
+            <TextDisplay weight='normal'>Name: {displayManufacturer.items[2].name}</TextDisplay>
+            <TextDisplay weight='normal'>
+              Made in: {displayManufacturer.items[2].originCountry}
+            </TextDisplay>
+            <TextDisplay weight='normal'>
+              Green Points: {formatNumber(displayManufacturer.items[2].greenPoint)}
+            </TextDisplay>
+            <TextDisplay weight='normal'>
+              Description: {displayManufacturer.items[2].description}
+            </TextDisplay>
+          </div>
+        </div>
       </div>
     </div>
   );
